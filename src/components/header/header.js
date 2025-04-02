bfe8354da6cde557a197a6a86820ac872f14f2ab
mizza-cz <mizza.cz666@gmail.com>
2025-04-02 10:50:04 +0200

    add favicon/tabs/dropdown

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,35 @@
 headerNavOpenerClick();
 function headerNavOpenerClick() {
-  const bodyEl = document.querySelector('body');
-  const headerNavOpener = document.querySelector('.js-header__opener');
+  const bodyEl = document.querySelector("body");
+  const headerNavOpener = document.querySelector(".js-header__opener");
   if (!bodyEl || !headerNavOpener) {
     return;
   }
-  headerNavOpener.addEventListener('click', function () {
-    if (!this.classList.contains('is-open')) {
-      bodyEl.classList.add('is-nav-open');
-      this.classList.add('is-open');
+  headerNavOpener.addEventListener("click", function () {
+    if (!this.classList.contains("is-open")) {
+      bodyEl.classList.add("is-nav-open");
+      this.classList.add("is-open");
     } else {
-      bodyEl.classList.remove('is-nav-open');
-      this.classList.remove('is-open');
+      bodyEl.classList.remove("is-nav-open");
+      this.classList.remove("is-open");
     }
   });
 }
+
+$(function () {
+  $(document).on("click", function (event) {
+    if (!$(event.target).closest(".nav-item").length) {
+      $(".nav-item").removeClass("active");
+    }
+  });
+
+  $(".nav-link").on("click", function (event) {
+    const $currentMenuList = $(this).closest(".nav-item");
+
+    $(".nav-item").not($currentMenuList).removeClass("active");
+
+    $currentMenuList.toggleClass("active");
+
+    event.stopPropagation();
+  });
+});
